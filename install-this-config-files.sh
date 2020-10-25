e72fe0c0d709c8ca44212582af1a5a782518e336
terremoth <dutr4@outlook.com>
2020-10-25 08:37:00 -0300

    test for credentials check - 2

--- install-this-config-files.sh
+++ install-this-config-files.sh
@@ -1,7 +1,5 @@
 #!/bin/env sh
 
-# this is free software
-
 for file in $(git ls-files); do 
     if [ $file != 'README.md' ] && [ $file != 'LICENSE' ] && [ $file != '.gitignore' ]; then 
         install -D $file ~/$file 
