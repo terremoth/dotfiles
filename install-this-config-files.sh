705c93f6be2ea18c945782f1453f4526f8cc12db
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-10-24 13:56:17 -0300

    Update install-this-config-files.sh

--- install-this-config-files.sh
+++ install-this-config-files.sh
@@ -8,7 +8,7 @@ done;
 
 if command -v git &> /dev/null && command -v base64 &> /dev/null; then
     git config --global user.name terremoth
-    git config --global user.email $(echo 'ZHV0cmEuYXN0cm9AZ21haWwuY29tCg==' | base64 -d)
+    git config --global user.email $(echo -n 'ZHV0cmEuYXN0cm9AZ21haWwuY29t' | base64 -d)
 fi
 
 echo 'All config files installed';
