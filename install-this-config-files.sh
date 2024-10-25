03d376be855ed3e24d5285c9609d4d546bcfc562
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-10-25 11:38:16 -0300

    Add oh my bash

--- install-this-config-files.sh
+++ install-this-config-files.sh
@@ -11,5 +11,11 @@ if command -v git &> /dev/null && command -v base64 &> /dev/null; then
     git config --global user.email $(echo -n 'ZHV0cmEuYXN0cm9AZ21haWwuY29t' | base64 -d)
 fi
 
+echo 'Updating bash profile'
+
+if command -v bash &> /dev/null && command -v wget &> /dev/null; then
+    bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+fi
+
 echo 'All config files installed';
 
